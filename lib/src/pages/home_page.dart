ca7b0b68a144ac1ef71f5d2cd7c9315398cb2d10
Geovanny Buitrago <geovanny91_1@hotmail.com>
2019-10-22 22:47:22 -0500

    Adding flutter_swiper library to add swiper stack to homepage.

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -6,9 +7,42 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Text('Home Page'),
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text('Movies at cinema'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: (){},
+          )
+        ],
       ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            _swipeCards()
+          ],
+        ),
+      )
+    );
+  }
+
+  Widget _swipeCards() {
+    return Container(
+      padding: EdgeInsets.all(10.0),
+      width: double.infinity,
+      height: 300,
+      child: Swiper(
+          itemBuilder: (BuildContext context,int index){
+            return Image.network("http://via.placeholder.com/350x150",
+              fit: BoxFit.fill
+            );
+          },
+          itemCount: 3,
+          pagination: SwiperPagination(),
+          itemWidth: 250,
+          layout: SwiperLayout.STACK,
+        ),
     );
   }
 }
