3214a7e1ec8996807a092f41ac1aa4a278690b00
Geovanny Buitrago <geovanny91_1@hotmail.com>
2019-10-23 23:17:35 -0500

    Integrating http library dart, and making movies provider class for fetching data from movie api.

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movies_app/src/providers/movies_provider.dart';
 import 'package:movies_app/src/widgets/card_swiper_widget.dart';
 
 class HomePage extends StatelessWidget {
@@ -28,6 +29,8 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _swipeCards() {
+    final _moviesProvider = MoviesProvider();
+    _moviesProvider.getNowPlaying();
     return CardSwiper(movies: [1,2,3,4,5]);
   }
 }
---
+++ lib/src/providers/movies_provider.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:movies_app/src/models/movie_model.dart';
+
+class MoviesProvider{
+
+    String _apiKey = '313c09930994b08741a0bc4bb9711cfc';
+    String _url = 'api.themoviedb.org';
+    String _language = 'en';
+
+
+    Future<List<Movie>> getNowPlaying() async{
+      final url = Uri.https(_url, '3/movie/now_playing',{
+        'api_key'   : _apiKey,
+        'language'  : _language
+      });
+      final response = await http.get(url);
+      final decodedData = jsonDecode(response.body);
+      print(decodedData);
+      final movies = Movies.fromJsonList(decodedData['results']);
+    }
+}
