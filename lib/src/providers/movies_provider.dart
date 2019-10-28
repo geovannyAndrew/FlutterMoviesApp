72a9be89ef0dbb184641362cf93fa466262f1b43
Geovanny Buitrago <geovanny.andrew911@gmail.com>
2019-10-27 21:16:13 -0500

    Creating stream for populars movies, defining sink to change values, and stream to listen the changes.

--- lib/src/providers/movies_provider.dart
+++ lib/src/providers/movies_provider.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:movies_app/src/models/movie_model.dart';
@@ -7,27 +8,47 @@ class MoviesProvider{
     String _apiKey = '313c09930994b08741a0bc4bb9711cfc';
     String _url = 'api.themoviedb.org';
     String _language = 'en';
+    int _pagePopulars = 0;
 
-    Future<dynamic> _get(String endPath) async{
-      final url = Uri.https(_url, endPath,{
-        'api_key'   : _apiKey,
-        'language'  : _language
-      });
+    List<Movie> _populars = new List();
+    final _popularsStreamController = StreamController<List<Movie>>.broadcast();
+
+    Function(List<Movie>) get popularsSink => _popularsStreamController.sink.add;
+
+    Stream<List<Movie>> get popularsStream => _popularsStreamController.stream; 
+
+    disposeStreams(){
+      _popularsStreamController?.close();
+    }
+
+    Future<dynamic> _get(String endPath, Map<String, String> params) async{
+      final url = Uri.https(_url, endPath,params);
       final response = await http.get(url);
       return jsonDecode(response.body);
     }
 
     Future<List<Movie>> getNowPlaying() async{
-      final decodedData = await _get('3/movie/now_playing');
+      final decodedData = await _get('3/movie/now_playing',{
+        'api_key'   : _apiKey,
+        'language'  : _language
+      });
       final movies = Movies.fromJsonList(decodedData['results']);
       return movies.items;
     }
 
     Future<List<Movie>> getPopulars() async{
-      final decodedData = await _get('3/movie/popular');
+      _pagePopulars++;
+      final decodedData = await _get('3/movie/popular',
+      {
+        'api_key'   : _apiKey,
+        'language'  : _language,
+        'page'      : _pagePopulars.toString(),
+      });
       final movies = Movies.fromJsonList(decodedData['results']);
+      _populars.addAll(movies.items);
+      popularsSink(_populars);
       return movies.items;
-    }
+    } 
 
 
 }
