005e372bcf4032b350f105da50ce38928667927f
Geovanny Buitrago <geovanny91_1@hotmail.com>
2019-10-29 23:26:27 -0500

    Mocking searching list of movies in order to undertand how search delegate works.

--- lib/src/search/search_delegate.dart
+++ lib/src/search/search_delegate.dart
@@ -2,6 +2,23 @@ import 'package:flutter/material.dart';
 
 class DataSearch extends SearchDelegate{
 
+  String _selectedMovie = '';
+
+  final movies = [
+    'Spiderman',
+    'Captain América',
+    'Batman',
+    'Movie other',
+    'Iron man',
+    'The mummy',
+    'Shazam'
+  ];
+
+  final moviesRecent = [
+    'Spiderman',
+    'Shazam'
+  ];
+
   @override
   List<Widget> buildActions(BuildContext context) {
     //Actions for Appbar
@@ -32,13 +49,37 @@ class DataSearch extends SearchDelegate{
   @override
   Widget buildResults(BuildContext context) {
     //Create results thta we want to show
-    return Container();
+    return Center(
+      child: Container(
+        height: 100,
+        width: 100,
+        color: Colors.blueAccent,
+        child: Text(_selectedMovie),
+      ),
+    );
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
     //Suggestions that appear when person writes
-    return Container();
+
+    final listSuggestions = query.isEmpty ? moviesRecent : movies.where((p)=>
+      p.toLowerCase().startsWith(query.toLowerCase())
+    ).toList();
+    return ListView.builder(
+      itemCount: listSuggestions.length,
+      itemBuilder: (BuildContext context, int index){
+        final movie = listSuggestions[index];
+        return ListTile(
+          leading: Icon(Icons.movie),
+          title: Text(movie),
+          onTap: (){
+            _selectedMovie = movie;
+            showResults(context);
+          },
+        );
+      },
+    );
   }
 
 }
